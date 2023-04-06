944d515dae5af7d01c52c3eccc3a0fc960897c21
Kaikaikaifang <2837968358@qq.com>
2023-04-06 16:27:12 +0800

    适配手机

--- src/layouts/PageHeader.vue
+++ src/layouts/PageHeader.vue
@@ -1,6 +1,6 @@
 <template>
   <header
-    class="fixed -ml-[17px] top-0 w-full h-14 bg-black flex justify-center"
+    class="fixed sm:-ml-[17px] top-0 w-full h-14 bg-black flex justify-center"
   >
     <div class="container h-full flex justify-start gap-3 items-center px-10">
       <img src="@/assets/soft-drinks.png" class="w-10" alt="logo" />
--- src/views/Background.vue
+++ src/views/Background.vue
@@ -8,7 +8,25 @@
       <p class="text-lg w-1/2">
         饮料机在现代社会中已经成为了人们生活中不可或缺❗️的一部分。然而，传统的饮料机存在一些问题❌，例如操作不方便、种类单一、卫生问题等。因此，我们团队决定设计一款新型的饮料机，以解决这些问题并提供更好的用户体验✅。
       </p>
-      <img src="@/assets/index.png" alt="index" class="w-[800px] rounded-lg" />
+      <t-swiper
+        class="tdesign-demo-block--swiper xl:w-[800px] w-[400px]"
+        :height="600"
+        :duration="300"
+        :interval="2000"
+      >
+        <t-swiper-item>
+          <img src="@/assets/index.png" alt="index" />
+        </t-swiper-item>
+        <t-swiper-item>
+          <img src="@/assets/hcoffee.png" alt="have a coffee" />
+        </t-swiper-item>
+        <t-swiper-item>
+          <img src="@/assets/hjuicy.png" alt="have a juicy" />
+        </t-swiper-item>
+        <t-swiper-item>
+          <img src="@/assets/hcola.png" alt="have a cola" />
+        </t-swiper-item>
+      </t-swiper>
     </div>
   </Layout>
 </template>
